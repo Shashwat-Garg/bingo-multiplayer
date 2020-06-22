e3df38a243cff337b3f49111589ef95baf698dee
Ezio-Sarthak <garg.sarthak13@gmail.com>
2020-06-22 21:31:32 +0530

    server-side.js implemented as backend for script.js

--- script.js
+++ script.js
@@ -1,58 +1,65 @@
-var arrBot = new Array(5);
-var arrUser = new Array(5);
-var row = 0,col = 0;
-for(var i = 0;i < 5;i++) {
-	arrUser[i] = new Array(5);
-	arrBot[i] = new Array(5);
-}
-for(var i = 0;i < 5;i++) {
-	for(var j = 0;j < 5;j++) {
-		arrUser[i][j] = 0;
-		arrBot[i][j] = 0;
-	}
-}
+// Row and column dynamic variables
+var row = 0, col = 0;
 
-//Player's Turn Variable
+// ********** Creating Socket *****
+var socket = io.connect("http://localhost:3000");
+
+// Add a connect listener
+socket.on('connect', function() {
+	console.log('Client has connected to the server!');
+});
+
+// Add a disconnect listener
+socket.on('disconnect', function() {
+	console.log('The client has disconnected!');
+});
+
+// ***** End of creating Socket *****
+
+// Player's turn variable
 var playerTurn;
 
-//Winning Player's Variable
-var userWon;
-var botWon;
+// Storing username
+var userName = document.getElementById("user-name").value;
+document.getElementById("user-name").value = "";
 
-//Creating Left Array And Used Array
-var leftArray = new Array(25);
-var usedArray = new Array(0);
-
-//Creating Input And Output Tables
+// Creating input and output tables
 var input = document.getElementById("input-table");
 var output = document.getElementById("output-table");
 outputInitializer(output);
 inputTable(input, output, "input_");
 
-// Creating Gaming And Bot's Table
+// Creating gaming gnd bot's table
 var gamingTable = document.getElementById("gaming-table");
-var botTable = document.getElementById("bot-table");
-
-// Gaming Object Dictionary
-var game = {"user-matrix": gamingTable, 
-			"bot-matrix": botTable, 
-			"left-numbers": leftArray, 
-			"used-numbers": usedArray};
-//Creating BINGO strings for user and bot
-var cntUser;
-var cntBot;
 
 // *****Functions******
 
-//Function To Check If Table Is Completely Filled
-function confCheck(page1, page2) {
-	if(row == 5 || col == 5)
-		clearScreen(page1, page2);
-	else
-		alert("Please Fill Matrix Completely !");
+// Function to check if username is correctly entered
+function confUser() {
+	userName = document.getElementById("user-name").value;
+	var JSobj = {
+		username: userName
+	};
+	socket.emit('userNameInput', JSobj, function(retObj) {
+		if(retObj.success) {
+			document.getElementById("page0").setAttribute("style","display: none;");
+			document.getElementById("page1").style.display = "inline-block";
+		}
+		else {
+			alert(retObj.error);
+		}
+	});
+}
+// Function to check if matrix is filled completely
+function confMatrix() {
+	if((row != 5) && (col != 5))
+		alert("Please fill matrix completely !");
+	else {
+		clearScreen();
+	}
 }
 
-//Function To Reset Input Table
+// Function to reset input table
 function resetTable() {
 	row = 0;
 	col = 0;
@@ -60,20 +67,21 @@ function resetTable() {
 	outputInitializer(output);
 	input.innerHTML = null;
 	inputTable(input, output, "input_");
+	var JSobj = {
+		username: userName
+	};
+	socket.emit('resetMatrix', JSobj);
 }
 
-//Function To Create Random Input Table
+// Function to create random input table
 function createRandomInput() {
-	output.innerHTML = null;
 	createRandomTable(output, "outputcell");
-	for(var i = 0;i < 5;i++)
-		for(var j = 0;j < 5;j++)
-			Disable(input.children[i].children[j]);
+	disableAll(input);
 	row = 5;
-	col = 5;	
+	col = 5;
 }
 
-//Initialising Output Table
+// Initialising output table
 function outputInitializer(table) {
 	var cnt = 1;
 	for(var i = 0;i < 5;i++) {
@@ -93,7 +101,7 @@ function outputInitializer(table) {
 	table.rows[0].cells[0].setAttribute("style", "color: black");	
 }
 
-//Input Table Function
+// Input table function
 function inputTable(table, outputTable, cellId) {
 	var cnt = 1;
 	for(var i = 0;i < 5;i++) {	
@@ -109,6 +117,11 @@ function inputTable(table, outputTable, cellId) {
 			td.className = "button button1";
 			td.id += cnt;
 			td.onclick = function() {
+				var JSobj = {
+					username: userName,
+					element:  parseInt(this.innerHTML, 10)
+				};
+				socket.emit('matrixElementInput', JSobj);
 				Disable(this);
 				AddArr(this, outputTable);
 			}
@@ -119,7 +132,7 @@ function inputTable(table, outputTable, cellId) {
 	}
 }
 
-//Output Table Based Input Table Function
+// Output table based input table function
 function outputBasedInputTable(table, knownTable) {
 	for(var i = 0;i < 5;i++) {	
 		var tr = document.createElement("tr");
@@ -132,8 +145,33 @@ function outputBasedInputTable(table, knownTable) {
 				if(playerTurn) {
 					playerTurn = false;
 					Disable(this);
-					play(this.innerHTML);
-					checkBingo(this.id[0], this.id[2], arrUser, cntUser, "u");
+					var obj = {
+						username: userName,
+						element: parseInt(this.innerHTML, 10)
+					};
+					socket.emit('userSelection', obj, function(data) {
+						if(data.success) {
+							playerTurn = true;
+							disableUserCell(data.element);
+							bingoTicks(data.countuser);
+							if((data.countuser == 5) || (data.countbot == 5)) {
+								disableAll(table);
+								document.getElementById("replay").disabled = false;
+							}
+							if((data.countuser == 5 ) && (data.countbot == 5)) {
+								document.getElementById("end-result").innerHTML = "It Was A Draw !!!";
+							}
+							else if(data.countuser == 5) {
+								document.getElementById("end-result").innerHTML = "You Won !!!";
+							}
+							else if(data.countbot == 5) {
+								document.getElementById("end-result").innerHTML = "Bot Won :(";
+							}
+						}
+						else {
+							alert(data.error);
+						}
+					});
 				}
 			}
 			tr.appendChild(td);
@@ -142,54 +180,67 @@ function outputBasedInputTable(table, knownTable) {
 	}
 }
 
-//Function To Create Random Table
-function createRandomTable(table, clName) {
-	var a = new Array(25);
-	for(var i = 0;i < 25;i++) {
-		a[i] = i + 1;
+// Function to show the current bingo ticks
+function bingoTicks(cnt) {
+	for(var i = 1;i <= cnt; i++) {
+		var obj = document.getElementById('u'+i);
+		if(obj.src.substring(obj.src.length - 8,obj.src.length) == "-CUT.png") {
+			continue;
+		}
+		document.getElementById('u'+i).src  = obj.src.substring(0,obj.src.length - 4)+"-CUT.png";
 	}
-	createRandom(a);
-	for(var i = 0;i < 25;i++) {
-		if(a[i] % 10 == a[i]) {
-			a[i] = "0" + a[i];
+}
+
+// Function to disable all buttons (called when game ends or when user selects random arrays)
+function disableAll(table) {
+	for(var i = 0;i < 5;i++) {
+		for(var j = 0;j < 5;j++) {
+			Disable(table.children[i].children[j]);
+		}
+	}
+}
+
+// Function to create random table
+function createRandomTable(table, clName) {
+	var JSobj = {
+		username: userName
+	};
+	socket.emit('requestRandomMatrix', JSobj, function(data) {
+		if(data.success) {
+			var a = Array.from(data.matrix);
+			for(var i = 0;i < 25;i++) {
+				if(a[i] % 10 == a[i]) {
+					a[i] = "0" + a[i];
+				}
+				else {
+					a[i] = a[i] + "";
+				}
+			}
+			var cnt = 0;
+			for(var i = 0;i < 5;i++) {
+				for(var j = 0;j < 5;j++) {
+					var td = table.rows[i].cells[j];
+					td.innerHTML = a[cnt];
+					td.id = cnt+1;
+					td.className = clName;
+					td.setAttribute("style", "color: black;");
+					cnt++;
+				}
+			}
 		}
 		else {
-			a[i] = a[i] + "";
+			alert(data.error);
 		}
-	}
-	var cnt = 0;
-	for(var i = 0;i < 5;i++) {	
-		var tr = document.createElement("tr");
-		for(var j = 0;j < 5;j++) {	
-			var td = document.createElement("td");		
-			td.innerHTML = a[cnt];
-			td.id = cnt+1;
-			td.className = clName;
-			cnt++;
-			tr.appendChild(td);
-		}
-		table.appendChild(tr);
-	}
+	});
 }
 
-//Function To Create Random Array
-function createRandom(a) {
-	var tmp,cur,tp = a.length;
-	while(--tp) {
-		cur = Math.floor(Math.random()*(tp + 1));
-		tmp = a[cur];
-		a[cur] = a[tp];
-		a[tp] = tmp;
-	}
-}
-
-//Function To Disable A Button
+//Function to disable a button
 function Disable(button) {
 	button.onclick = null;
 	button.className += " disabled_button";
 }
 
-//Function To Add Element To Output Table
+// Function to add element to output able
 function AddArr(button, outputTable) {
 	outputTable.rows[row].cells[col].innerHTML = button.innerHTML;
 	outputTable.rows[row].cells[col].setAttribute("style","color: black");
@@ -205,161 +256,36 @@ function AddArr(button, outputTable) {
 	outputTable.rows[row].cells[col].setAttribute("style", "text-align: center; color: black");
 }
 
-//Clearing Current Page And Showing Next Page
-function clearScreen(page1,page2) {
-	document.getElementById(page1).setAttribute("style","display: none;");
-	document.getElementById(page2).setAttribute("style","display: inline-block");
+// Function to proceed to next page
+function clearScreen() {
+	document.getElementById("page1").setAttribute("style","display: none;");
+	document.getElementById("page2").setAttribute("style","display: inline-block");
 	row = 0;
 	col = 0;
 	playerTurn = true;
-	for(var i = 1;i <= 25;i++) {
-		leftArray[i-1] = i;
-	}
-	cntBot = [0];
-	cntUser = [0];
-	userWon = false;
-	botWon = false;
-	createRandomTable(botTable, "table2");
-	outputBasedInputTable(gamingTable, output);
+	var obj = {
+		username: userName
+	};
+	socket.emit('createBotMatrix', obj, function(data) {
+		if(data.success) {
+			outputBasedInputTable(gamingTable, output);
+		}
+		else {
+			alert(data.error);
+		}
+	});
 }
 
-//Function To Play With Bot
-function play(value) {
-	if(leftArray.length == 0)
-		return;
-	for(var i = 0;i < 25;i++) {
-			if(leftArray[i] == parseInt(value, 10)) {
-				usedArray.push(parseInt(value, 10));
-				leftArray.splice(i, 1);
-		}
-	}
-	for(var i = 0;i < 5;i++) {
-		for(var j = 0;j < 5;j++) {
-			if(botTable.rows[i].cells[j].innerHTML == value) {
-				botTable.rows[i].cells[j].setAttribute("style", "color: white;");
-				checkBingo(i, j, arrBot, cntBot, "b");
-			}
-		}
-	}
-	setTimeout(function (){
-		playBot(value);
-		playerTurn = true;
-	}, 700);
-}
 
-//Function To Track Bot's Move
-function playBot(value) {
-	var randIndex = Math.floor(Math.random()*(leftArray.length));
-	var ele = leftArray[randIndex];
-	usedArray.push(leftArray[randIndex]);
-	leftArray.splice(randIndex, 1);
-	var target = "";
-	if(ele % 10 == ele)
-		target += "0";
-	target = target + ele;
+// Function to disable user's table cell
+function disableUserCell(target) {
 	for(var i = 0;i < 5;i++) {
 		for(var j = 0;j < 5;j++) {
 			if(gamingTable.children[i].children[j].innerHTML == target) {
 				Disable(gamingTable.children[i].children[j]);
-				checkBingo(i, j, arrUser, cntUser, "u");
-			}
-			if(botTable.rows[i].cells[j].innerHTML == target) {
-				botTable.rows[i].cells[j].setAttribute("style", "color: white;");
-				checkBingo(i, j, arrBot, cntBot, "b");
-			}
-		}
-	}
-}
-
-//Function To Print End Result
-
-// Function To Check BINGO Status
-function checkBingo(r, c, arr, cnt, ch) {
-	r = parseInt(r, 10);
-	c = parseInt(c, 10);
-	arr[r][c] = 1;
-	var frow = 1,fcol = 1;
-	for(var i = 0;i < 5;i++) {
-		if(arr[r][i] == 0) {
-			frow = 0;
-			break;
-		}
-	}
-	for(var i = 0;i < 5;i++) {
-		if(arr[i][c] == 0) {
-			fcol = 0;
-			break;
-		}
-	}
-	if(frow == 1) {
-		cnt[0]++;
-		if(document.getElementById(ch+cnt[0])) {
-			var obj = document.getElementById(ch+cnt[0]);
-			document.getElementById(ch+cnt[0]).src  = obj.src.substring(0,52)+"-CUT.png";
-		}
-	}
-	if(fcol == 1 && cnt[0] < 5) {
-		cnt[0]++;
-		if(document.getElementById(ch+cnt[0])) {
-			var obj = document.getElementById(ch+cnt[0]);
-			document.getElementById(ch+cnt[0]).src  = obj.src.substring(0,52)+"-CUT.png";
-		}
-	}
-	if(r == c) {
-		var fdiagLeft = 1;
-		for(var i = 0;i < 5;i++) {
-			if(arr[i][i] == 0) {
-				fdiagLeft = 0;
 				break;
 			}
 		}
-		if(fdiagLeft == 1 && cnt[0] < 5) {
-			cnt[0]++;
-			if(document.getElementById(ch+cnt[0])) {
-				var obj = document.getElementById(ch+cnt[0]);
-				document.getElementById(ch+cnt[0]).src  = obj.src.substring(0,52)+"-CUT.png";
-			}
-		}
-	}
-	if((r + c) == 4) {
-		var fdiagRight = 1;
-		for(var i = 0;i < 5;i++) {
-			if(arr[i][4 - i] == 0) {
-				fdiagRight = 0;
-				break;
-			}
-		}
-		if(fdiagRight == 1 && cnt[0] < 5){
-			cnt[0]++;
-			if(document.getElementById(ch+cnt[0])) {
-				var obj = document.getElementById(ch+cnt[0]);
-				document.getElementById(ch+cnt[0]).src  = obj.src.substring(0,52)+"-CUT.png";
-			}
-		}
-	}
-	if(cnt[0] == 5) {
-		document.getElementById("replay").disabled = false;
-		for(var i = 0;i < 5;i++) {
-			for(var j = 0;j < 5;j++) {
-				botTable.rows[i].cells[j].setAttribute("style", "color: white;");
-				Disable(gamingTable.children[i].children[j]);
-			}
-		}
-		if(ch == "u") {
-			userWon = true;
-			return;
-		}
-		else {
-			botWon = true;
-			if(userWon == true) {
-				document.getElementById("end-result").innerHTML = "It Was A Draw !!!";
-				return;
-			}
-			document.getElementById("end-result").innerHTML = "Bot Won :(";
-		}
-	}
-	if(userWon == true) {
-		document.getElementById("end-result").innerHTML = "You Won !!!";
 	}
 }
 
